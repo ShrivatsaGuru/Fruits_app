d9c454ac9570d6150cad5f3cdce1343797b509f0
ShrivatsaGuru <150587011+ShrivatsaGuru@users.noreply.github.com>
2025-02-12 16:21:49 +0530

    6 Pages

--- lib/components/basket_item.dart
+++ lib/components/basket_item.dart
@@ -4,10 +4,5 @@ class BasketItem {
   final int price;
   final int quantity;
 
-  BasketItem({
-    required this.name,
-    required this.image,
-    required this.price,
-    required this.quantity,
-  });
+ 
 }
